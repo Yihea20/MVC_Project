614d093ef133da0a452989457c14df7584d10308
Yihea20 <111204473+Yihea20@users.noreply.github.com>
2023-10-14 10:02:47 +0300

    Identity

--- MVC_Project/Areas/Customer/Controllers/HomeController.cs
+++ MVC_Project/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.DataAccess.IRebository;
 using MVC.Model;
 using System.Diagnostics;
 
@@ -7,16 +10,24 @@ namespace MVC_Project.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork,IMapper mapper)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
-            return View();
+           var  productlist = await _unitOfWork.Product.GetAll(include: q => q.Include(x => x.Category));
+            return View(productlist);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _unitOfWork.Product.Get(q=>q.Id==id,include: q => q.Include(x => x.Category));
+            return View(product);
         }
 
         public IActionResult Privacy()
--- MVC_Project/Program.cs
+++ MVC_Project/Program.cs
@@ -3,12 +3,15 @@ using MVC.DataAccess;
 using MVC.DataAccess.IRebository;
 using MVC.DataAccess.Rebository;
 using MVC.Model.Entity;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("MVC_Data")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddAutoMapper(typeof(MapperInitilizer));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
